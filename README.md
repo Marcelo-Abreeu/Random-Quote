49d921eace942db4abbc93b473a768e5213a896c
Marcelo <110702843+kamilli-abreu@users.noreply.github.com>
2022-11-17 10:05:51 -0300

    Terminado

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# Random-Quote
- Random phrase generator
+# Gerador de Frases
+
+Visualizar Projeto: [Clique aqui](https://github.com)
+<br>
+
+## Layoutdo Projeto
+![](preview/Prewien.png)
+
+
+## Sobre o Projeto
+
+Neste projeto utilizei uma API com arquivos .JSON. 
+As Frases são puxadas de forma dinamica e mostradas ao usúario.
+O mesmo retorna frases de forma aleatória com citações de grandes filósofos, empresários e jogadores de futebol.
+<br>
+
+## Aprendizados 
+- Criação de .JSON
+- Consumos de API
+
+
---
+++ script.js
@@ -0,0 +1,42 @@
+function init(SeletorFrase, seletorAutor, seletorBtn) {
+    // Selecionando elementos do DOM
+    const frase = document.querySelector(SeletorFrase);
+    const autor = document.querySelector(seletorAutor);
+    const btn = document.querySelector(seletorBtn);
+    const body = document.querySelector('body');
+
+    // Tratativa de erro
+    if (frase && autor && btn) {
+        // Função Assincrona puxando a frase da API
+        async function activeApp() {
+            try {
+                // Frase API
+
+                // Faz um fetch na url
+                const dadosResponse = await (fetch('./frases.json'));
+                // Aguarda o retorno do Fetch e transforma em JSON
+                const dadosJSON = await (await dadosResponse).json();
+                // Puxando as frases de forma aleatoria
+                const aleatorio = dadosJSON[Math.floor(Math.random() * 288)];
+
+                // Insere os dados no DOM
+                frase.innerText = aleatorio.quote;
+                autor.innerText = aleatorio.author;
+
+            } catch (erro) {
+                console.log(erro);
+            }
+
+        }
+
+        // Evento do botão
+        btn.addEventListener('click', activeApp);
+
+        // Ativando a função quando entra no site
+        activeApp();
+    }
+
+
+}
+// Chamando a função geral para inicar o codigo
+init('.frase', '.autor', '.btn-novo');
